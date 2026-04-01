6452ef4e83d6f26ee5e4e600c4892381304fa037
pedro <l2pedro157@gmail.com>
2026-04-01 17:13:16 -0300

    fix: adicionado validaçoes para não nulo no usuario request

--- src/main/java/br/com/tasklist/dto/request/UsuarioRequest.java
+++ src/main/java/br/com/tasklist/dto/request/UsuarioRequest.java
@@ -2,12 +2,13 @@ package br.com.tasklist.dto.request;
 
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CPF;
 
 
 public record UsuarioRequest(@Size(max = 150,message = "Deve ter no maximo 150 caracteres.") String nome,
-                             @Email(message = "Formato inválido.") String email,
-                             @CPF(message = "Formato inválido") String cpf,
+                             @Email(message = "Formato inválido.")@NotNull(message = "É obrigatório.") String email,
+                             @CPF(message = "Formato inválido")@NotNull(message = "É obrigatório.") String cpf,
                              @Size(max = 300,message = "Deve ter no maximo 300 caracteres.") String imgUrlPerfil) {
 }
